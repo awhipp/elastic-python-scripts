9edd198ba372610681b3ab3b380a18a42d6d5edf
Alexander Whipp <alexruns4@gmail.com>
2019-08-30 15:29:51 -0400

    Multithreaded

--- upload-file-to-elastic.py
+++ upload-file-to-elastic.py
@@ -1,13 +1,18 @@
 from elasticsearch import Elasticsearch
 import json
 import numbers
+            
+from multiprocessing.pool import ThreadPool as Pool
+from random import randint
+from time import sleep
 
+# CONFIG
 ELASTIC_HOST = '127.0.0.1'
 ELASTIC_PORT = 9200
 ELASTIC_IDX = 'INDEX_NAME'
 ELASTIC_DOC_TYPE = 'DOC_TYPE'
-
-FILE_NAME = "file.json"
+CPU_THREADS = 4
+FILE_NAME = "FILE.json"
 
 # CONNECTS TO ELASTIC SEARCH
 es = Elasticsearch([{'host': ELASTIC_HOST, 'port': ELASTIC_PORT}])
@@ -15,17 +20,33 @@ es = Elasticsearch([{'host': ELASTIC_HOST, 'port': ELASTIC_PORT}])
 # DELETES THE INDEX IF IT ALREADY EXISTS
 es.indices.delete(index=ELASTIC_IDX, ignore=[400, 404])
 
-with open(FILE_NAME) as fp:
-    line = fp.readline()
-    cnt = 1
-    while line:
-        line = fp.readline()
-        jsonObj = json.loads(line)
-        try:
-            es.index(index=ELASTIC_IDX, doc_type=ELASTIC_DOC_TYPE, id=cnt, body=jsonObj)
-        except:
-            print("Failed on {}: {}".format(cnt, line.strip()))
-        finally:
+###
+# THREADED INSERT
+###
+
+# Process Line
+def process_line(line):
+    jsonObj = json.loads(line)
+    try:
+        es.index(index=ELASTIC_IDX, doc_type=ELASTIC_DOC_TYPE, body=jsonObj)
+    except:
+        print("Failed: {}".format(line.strip()))
+
+
+# Get next line
+def get_next_line():
+    with open(FILE_NAME, 'r') as f:
+        cnt = 0
+        for line in f:
+            yield line
             cnt += 1
-        
-print("Uploaded {}".format(cnt))
+            if cnt % 1000 == 0:
+                print(cnt)
+
+# Thread management
+f = get_next_line()
+t = Pool(processes=CPU_THREADS)
+for i in f:
+    t.map(process_line, (i,))
+t.join()
+t.close()
